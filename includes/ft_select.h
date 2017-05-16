1f6d46294910c0fe7c7ac719235ce21476aa8187
Sam Escolas <samescolas@gmail.com>
2017-05-16 15:19:18 -0700

    Adding ft_select.h

---
+++ includes/ft_select.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_select.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/16 10:49:26 by sescolas          #+#    #+#             */
+/*   Updated: 2017/05/16 10:51:12 by sescolas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_SELECT_H
+# define FT_SELECT_H
+
+# define DEF "\x1B[0m"
+# define RED "\x1B[31m"
+# define BLU "\x1B[34m"
+# define MAG "\x1B[35m"
+# define CY1 "\x1B[36m"
+# define CY2 "\x1B[37m"
+# define CY3 "\x1B[96m"
+# define BK2 "\x1B[90m"
+# define BL2 "\x1B[94m"
+# define MG2 "\x1B[95m"
+
+#endif
