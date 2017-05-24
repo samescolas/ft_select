88f844e09e14390ed7dcc65079064c3544dfaa38
Sam Escolas <samescolas@gmail.com>
2017-05-23 18:04:05 -0700

    Added get_max_length function to main (to be moved)

--- srcs/main.c
+++ srcs/main.c
@@ -6,21 +6,40 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/16 10:23:04 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/23 17:24:03 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/23 18:01:19 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "init.h"
 #include "ft_atexit.h"
+#include "ft_types.h"
 
 #include "../libft/libft.h"
 
+int		get_max_length(char **arr)
+{
+	int		max;
+	int		len;
+	int		i;
+
+	i = 0;
+	max = ft_strlen(arr[i]);
+	while (arr[++i])
+		if ((len = ft_strlen(arr[i])) > max)
+			max = len;
+	return (max);
+}
+
 int		main(int argc, char **argv)
 {
+	t_window	*term;
+
 	if (argc > 1)
 	{
-		load_terminal();
+		term = load_terminal();
+		ft_putnbr(get_max_length(&argv[1]));
+		write(1, "\n\r", 2);
 		ft_atexit();
 	}
-	return (!argv[0]);
+	return (0);
 }
