2f31eac35cb6e7375ff9e3b6aa10ee1cdd5ab96c
Sam Escolas <samescolas@gmail.com>
2017-05-24 15:18:37 -0700

    the main so far..

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/16 10:23:04 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/23 18:01:19 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/24 14:38:19 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,10 @@
 #include "ft_atexit.h"
 #include "ft_types.h"
 
+#include "ft_terminfo.h"
+#include "dimensionalize.h"
+#include <stdio.h>
 #include "../libft/libft.h"
-
-int		get_max_length(char **arr)
-{
-	int		max;
-	int		len;
-	int		i;
-
-	i = 0;
-	max = ft_strlen(arr[i]);
-	while (arr[++i])
-		if ((len = ft_strlen(arr[i])) > max)
-			max = len;
-	return (max);
-}
 
 int		main(int argc, char **argv)
 {
@@ -36,9 +25,8 @@ int		main(int argc, char **argv)
 
 	if (argc > 1)
 	{
-		term = load_terminal();
-		ft_putnbr(get_max_length(&argv[1]));
-		write(1, "\n\r", 2);
+		term = load_terminal(argc - 1);
+		test_dimensions(argv, term);
 		ft_atexit();
 	}
 	return (0);
