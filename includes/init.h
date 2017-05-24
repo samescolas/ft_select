c16d0fdd4cf083c0d85bebe274036e31da85416d
Sam Escolas <samescolas@gmail.com>
2017-05-24 15:17:36 -0700

    Added num_args to this function to preload t_win with value

--- includes/init.h
+++ includes/init.h
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 14:18:46 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/23 17:53:40 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/24 10:38:12 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@
 typedef struct s_window t_window;
 
 void			reset_terminal(void);
-t_window		*load_terminal(void);
+t_window		*load_terminal(int num_args);
 
 #endif
