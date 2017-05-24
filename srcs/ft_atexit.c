fed1d72423555e4c3df6fa144e8510cd9cd2fc90
Sam Escolas <samescolas@gmail.com>
2017-05-23 17:31:19 -0700

    Added function to load void functions to be called upon exit

--- srcs/ft_atexit.c
+++ srcs/ft_atexit.c
@@ -6,14 +6,14 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/16 11:38:18 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/23 14:24:32 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/23 17:23:18 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "init.h"
+#include "ft_atexit.h"
 #include "../libft/libft.h"
 
-static void	resize_arr(char ***arr, int size, int buff_size)
+static void	resize_str_arr(char ***arr, int size, int buff_size)
 {
 	char	**ret;
 	int		i;
@@ -28,36 +28,82 @@ static void	resize_arr(char ***arr, int size, int buff_size)
 	*arr = ret;
 }
 
-void	ft_str_atexit(int loading, char *str_to_free)
+void		ft_str_atexit(int loading, char *str_to_free)
 {
 	static char	**string_arr;
 	static int	num_strings;
-	int			buff_size;
 	int			i;
 
-	buff_size = 10;
 	i = -1;
-	if (num_strings == 0)
-		if (!(string_arr = (char **)malloc(buff_size * sizeof(char *))))
+	if (!num_strings)
+		if (!(string_arr = (char **)malloc(STRING_BUFFER * sizeof(char *))))
 			ft_fatal("err: out of memory\n");
 	if (loading)
 	{
-		if (num_strings > 0 && num_strings % 10 == 0)
-			resize_arr(&string_arr, num_strings, buff_size);
+		write(1, "\r", 1);
+		if (num_strings > 0 && num_strings % STRING_BUFFER == 0)
+			resize_str_arr(&string_arr, num_strings, STRING_BUFFER);
 		string_arr[num_strings++] = str_to_free;
 	}
 	else
 	{
 		while (++i < num_strings)
-			ft_strdel(&string_arr[i]);
+		{
+			ft_bzero(string_arr[i], sizeof(string_arr[i]));
+			free(string_arr[i]);
+			string_arr[i] = (void *)0;
+		}
 		free(string_arr);
 		string_arr = (void *)0;
+	}
+}
+
+static void	resize_func_arr(void (***arr)(void), int size, int buff_size)
+{
+	void	(**ret)(void);
+	int		i;
+
+	if (!(ret = (void (**)(void))malloc((size + buff_size) * sizeof(void (*)(void)))))
+		ft_fatal("err: out of memory\n");
+	i = -1;
+	while (++i < size)
+		ret[size] = (*arr)[size];
+	ret[size] = (void *)0;
+	free(*arr);
+	*arr = ret;
+}
+
+void		ft_func_atexit(int loading, void (*f)(void))
+{
+	static void	(**func_arr)(void);
+	static int	num_funcs;
+	int			i;
+
+	i = -1;
+	if (num_funcs == 0)
+		if (!(func_arr = (void (**)(void))malloc(FUNCTION_BUFFER * sizeof(void (*)(void)))))
+			ft_fatal("err: out of memory\n");
+	if (loading)
+	{
+		if (num_funcs > 0 && num_funcs % FUNCTION_BUFFER == 0)
+			resize_func_arr(&func_arr, num_funcs, FUNCTION_BUFFER);
+		func_arr[num_funcs++] = f;
+	}
+	else
+	{
+		while (++i < num_funcs)
+		{
+			func_arr[i]();
+			func_arr[i] = (void *)0;
+		}
+		free(func_arr);
+		func_arr = (void *)0;
 	}
 }
 
 void	ft_atexit(void)
 {
 	ft_str_atexit(0, (void *)0);
-	reset_terminal_settings((void *)0);
+	ft_func_atexit(0, (void *)0);
 	exit(0);
 }
