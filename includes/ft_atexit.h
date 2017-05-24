5d82be75d70c9e6bdbb1eb0b5d23bdeec9d0ceef
Sam Escolas <samescolas@gmail.com>
2017-05-23 17:27:23 -0700

    Cleaned up code

--- includes/ft_atexit.h
+++ includes/ft_atexit.h
@@ -6,14 +6,18 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/16 11:48:25 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/16 11:50:29 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/23 17:00:32 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ATEXIT_H
 # define FT_ATEXIT_H
 
+# define STRING_BUFFER 5
+# define FUNCTION_BUFFER 5
+
 void	ft_atexit(void);
 void	ft_str_atexit(int loading, char *str_to_free);
+void	ft_func_atexit(int loading, void (*f)(void));
 
 #endif
