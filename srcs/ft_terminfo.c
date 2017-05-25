38bb3aa588ad356322e11dc62459e751a84b4086
Sam Escolas <samescolas@gmail.com>
2017-05-25 10:32:07 -0700

    Made changes to add things to border_left

--- srcs/ft_terminfo.c
+++ srcs/ft_terminfo.c
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 17:55:55 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/24 11:51:38 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/24 15:59:21 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "ft_select.h"
 #include "ft_types.h"
 
-int		get_term_width(void)
+size_t	get_term_width(void)
 {
 	int	ret;
 
@@ -23,7 +23,7 @@ int		get_term_width(void)
 	return (0);
 }
 
-int		get_term_height(void)
+size_t	get_term_height(void)
 {
 	int	ret;
 
@@ -32,23 +32,33 @@ int		get_term_height(void)
 	return (0);
 }
 
-int		check_term_height(t_window win)
+size_t	get_content_height(t_window win)
 {
-	int		rows;
 	size_t	height;
+	size_t	num_rows;
 
-	rows = ft_ceil(win.num_args, win.num_cols);
+	num_rows = ft_ceil(win.num_args, win.num_cols);
 	height = win.border_top;
-	height = rows * win.box_height;
-	height += (rows - 1) * win.v_padding;
-	return (height <= win.term_height);
+	height += (num_rows * win.box_height);
+	height += ((win.num_args - 1) * win.v_padding);
+	return (height);
 }
 
-int		check_term_width(t_window win)
+size_t	get_content_width(t_window win)
 {
 	size_t	width;
 
 	width = win.num_cols * win.box_width;
-	width += (win.num_cols - 1) * win.h_padding;
-	return (width <= win.term_width);
+	width += ((win.num_cols - 1) * win.h_padding);
+	return (width);
+}
+
+int		check_term_height(t_window win)
+{
+	return (get_content_height(win) <= win.term_height);
+}
+
+int		check_term_width(t_window win)
+{
+	return (get_content_width(win) <= win.term_width);
 }
