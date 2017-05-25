ce0b128202c94e209305b87fd5bb4fa3ee1d2067
Sam Escolas <samescolas@gmail.com>
2017-05-25 10:31:01 -0700

    Added border_left to t_windor, and added two new structs for the choices and thier coordinates

--- includes/ft_types.h
+++ includes/ft_types.h
@@ -6,24 +6,23 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 17:46:26 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/24 10:35:20 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/25 09:28:09 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_TYPES_H
 # define FT_TYPES_H
 
-typedef struct	s_coord
-{
-	size_t		x;
-	size_t		y;
-}				t_coord;
+# include <stdlib.h>
+
+typedef struct s_choice	t_choice;
 
 typedef struct	s_window
 {
 	size_t	term_width;
 	size_t	term_height;
 	size_t	border_top;
+	size_t	border_left;
 	size_t	box_width;
 	size_t	box_height;
 	size_t	h_padding;
@@ -33,6 +32,25 @@ typedef struct	s_window
 	
 }				t_window;
 
+typedef struct	s_coord
+{
+	size_t		x;
+	size_t		y;
+}				t_coord;
+
+typedef struct	s_choice
+{
+	char		*label;
+	size_t		len;
+	t_coord		*position;
+	size_t		selected;
+	t_choice	*next;
+	t_choice	*prev;
+}				t_choice;
+
 t_window	*ft_create_window(int num_args);
+t_choice	*create_choice(char *label, t_coord *position);
+t_coord		*create_coord(size_t x, size_t y);
+void		choice_add(t_choice **list, t_choice *item);
 
 #endif
