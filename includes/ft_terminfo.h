4386d9718fd7e16e6816e4689446de66fa7dea0e
Sam Escolas <samescolas@gmail.com>
2017-05-25 10:30:22 -0700

    Changed return values from int to size_t

--- includes/ft_terminfo.h
+++ includes/ft_terminfo.h
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 17:56:32 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/24 11:48:44 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/24 15:59:47 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 
 typedef struct s_window	t_window;
 
-int		get_term_width(void);
-int		get_term_height(void);
-int		check_term_width(t_window win);
-int		check_term_height(t_window win);
+size_t	get_term_width(void);
+size_t	get_term_height(void);
+size_t	get_content_width(t_window win);
+size_t	get_content_height(t_window win);
+size_t	check_term_width(t_window win);
+size_t	check_term_height(t_window win);
 
 #endif
