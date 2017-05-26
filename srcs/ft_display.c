99fc0dba2e4297ef769788fe6578f853b3f3bf5d
Sam Escolas <samescolas@gmail.com>
2017-05-25 18:46:24 -0700

    Prints list items in proper locations

--- srcs/ft_display.c
+++ srcs/ft_display.c
@@ -6,11 +6,17 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/25 09:43:54 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/25 09:47:46 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/25 11:43:29 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_types.h"
+#include "ft_termcap.h"
+
+
 void	display_choice(t_choice choice, int active)
 {
-	
+	ft_print_loc(choice.label, choice.position);
+	if (active)
+		return ;
 }
