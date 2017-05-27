b2b1f09eddf4e2ae8e0cda53171ff65b2654fa0b
Sam Escolas <samescolas@gmail.com>
2017-05-26 17:00:25 -0700

    Moved call to test_dimensions into dimensionalize

--- includes/dimensionalize.h
+++ includes/dimensionalize.h
@@ -6,14 +6,13 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/24 11:55:32 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/24 15:31:33 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/26 11:32:22 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DIMENSIONALIZE_H
 # define DIMENSIONALIZE_H
 
-void	test_dimensions(char **arg_list, t_window *window);
-void	dimensionalize(t_window *win);
+void	dimensionalize(char **args, t_window *win);
 
 #endif
